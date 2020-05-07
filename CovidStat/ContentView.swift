db0d8ff08ba9245fb0dba11c30c7c30cf1898a9a
Alexander Kraev <lexkraev@gmail.com>
2020-05-07 11:36:44 +0300

    Initial Commit

---
+++ CovidStat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CovidStat
+//
+//  Created by Alexander Kraev on 07.05.2020.
+//  Copyright © 2020 Alexander Kraev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
