a8502b9dea8d5636e396c359d817c5d535833eed
Alexander Kraev <lexkraev@gmail.com>
2020-05-10 22:56:49 +0300

    Add app info

--- CovidStat/Views/AboutView.swift
+++ CovidStat/Views/AboutView.swift
@@ -9,8 +9,54 @@
 import SwiftUI
 
 struct AboutView: View {
+    
+    func linkButton(link: String, buttonText: String) -> AnyView{
+        
+        return AnyView(Button(action: {
+            UIApplication.shared.open(URL(string: link)!)
+        }) {
+            Text(buttonText)
+        })
+    }
+    let email = "lexkraev@gmail.com"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading){
+            Group{
+                Spacer()
+                Text("About").font(.largeTitle)
+                Spacer()
+                
+                Text("This is an open source project")
+                
+                
+                linkButton(link: "https://github.com/c-villain/CovidStatApp", buttonText: "https://github.com/c-villain/CovidStatApp").font(.headline)
+                Spacer()
+                HStack{
+                    Text("based on ")
+                    linkButton(link: "https://covid19api.com", buttonText: "https://covid19api.com").font(.headline)
+                }
+                Spacer()
+                Text("@Alexander Kraev, 2020")
+                
+            }
+            Group{
+                Spacer()
+                Spacer()
+                Spacer()
+                Text("Contact").font(.largeTitle)
+                Spacer()
+                Text("If you have any questions or suggestions for improvement, please contact me:")
+                linkButton(link: "t.me/cvillain", buttonText: "t.me/cvillain").font(.headline)
+            }
+            HStack{
+                Text("or email me:")
+                linkButton(link: "mailto:\(email)", buttonText: "lexkraev@gmail.com").font(.headline)
+            }
+            Spacer()
+            Spacer()
+            Spacer()
+        }
     }
 }
 
